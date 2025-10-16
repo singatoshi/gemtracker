d7c7d2d18cda4f04cf1b3399161fc882409c8ff9
Singatoshi <34945499+singatoshi@users.noreply.github.com>
2025-10-16 05:22:39 -0400

    Update RewardToken.sol

--- contracts/RewardToken.sol
+++ contracts/RewardToken.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.17;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 contract RewardToken is ERC20, Ownable {
     constructor(uint256 initialSupply) ERC20("SocialFi Token", "SFT") {
